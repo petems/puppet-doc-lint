15756a6ba792337fc90f4fd305151d2e7be27b82
Peter Souter <p.morsou@gmail.com>
2014-02-11 19:14:58 +0000

    Refactored the result class I've given it a native method to do a verbose output of the details

--- lib/puppet-doc-lint/result.rb
+++ lib/puppet-doc-lint/result.rb
@@ -6,8 +6,18 @@ class PuppetDocLint
     attribute :class_name, String
     attribute :file_name, String
     attribute :parameters, String
-    attribute :no_documentation, Boolean
-    attribute :documented_parameters,   String
-    attribute :undocumented_parameters, String
+    attribute :no_documentation, Boolean, :default => false
+    attribute :documented_parameters,   String, :default => []
+    attribute :undocumented_parameters, String, :default => []
+
+    def result_report
+      puts "Class name was: #{class_name}"
+      puts "File name #{file_name}"
+      puts "Parameters found #{parameters}"
+      puts "No documentation error: #{no_documentation}"
+      puts "Documented parameters found: #{documented_parameters}"
+      puts "Undocumented parameters found: #{undocumented_parameters}\n\n"
+    end
+
   end #class Result
 end #module PuppetDocLint
