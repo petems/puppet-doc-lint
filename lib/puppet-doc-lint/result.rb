3f3a71df833f856dc6494763c64ab18e81252b3a
Peter Souter <p.morsou@gmail.com>
2014-02-18 20:37:24 +0000

    Pretty up that exec!
    Should probably move most of the exec code into it's own class

--- lib/puppet-doc-lint/result.rb
+++ lib/puppet-doc-lint/result.rb
@@ -11,12 +11,28 @@ class PuppetDocLint
     attribute :undocumented_parameters, String, :default => []
 
     def result_report
-      puts "Class name was: #{class_name}"
-      puts "File name #{file_name}"
+      puts "===================================="
+      puts "Class #{class_name} ( #{file_name} )".bg_blue
       puts "Parameters found #{parameters}"
-      puts "No documentation error: #{no_documentation}"
-      puts "Documented parameters found: #{documented_parameters}"
-      puts "Undocumented parameters found: #{undocumented_parameters}\n\n"
+      if no_documentation
+        puts "No documentation found."
+        puts "If there is documentation, this may be a bug with the Puppet parser"
+        puts "Puppet files with newer features such as the use of hashes can cause this\n\n"
+      else
+        puts "Documented parameters found: #{documented_parameters}".green
+        puts "Undocumented parameters found: #{undocumented_parameters}".red
+        puts "Parameters with Documentation but no defintion: #{documented_parameter_no_assignment}".red unless authors.empty?
+        puts "Documentation Coverage: #{percent_documented}%"
+        puts "Authors: #{authors}" unless authors.empty?
+      end
+    end
+
+    def percent_documented
+      if documented_parameters.size == 0
+        100
+      else
+        documented_parameters.size.percent_of(parameters.size).round(2)
+      end
     end
 
   end #class Result
